6754c3bf733bf211c18d10ba993644f4233566be
br8bit <>
2025-02-03 15:31:13 +0400

    fix stack issue

--- programs/anchor-escrow/src/instructions/refund.rs
+++ programs/anchor-escrow/src/instructions/refund.rs
@@ -10,14 +10,15 @@ use crate::state::EscrowState;
 pub struct Refund<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
-    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    pub mint_a: Box<InterfaceAccount<'info, Mint>>,
 
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = maker,
     )]
-    pub maker_ata_mint_a: InterfaceAccount<'info, TokenAccount>,
+    pub maker_ata_mint_a: Box<InterfaceAccount<'info, TokenAccount>>,
 
     #[account(
         mut,
@@ -27,14 +28,14 @@ pub struct Refund<'info> {
         seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
         bump = escrow.bump
     )]
-    pub escrow: Account<'info, EscrowState>,
+    pub escrow: Box<Account<'info, EscrowState>>,
 
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = escrow,
     )]
-    pub vault: InterfaceAccount<'info, TokenAccount>,
+    pub vault: Box<InterfaceAccount<'info, TokenAccount>>,
 
     pub token_program: Interface<'info, TokenInterface>,
 
